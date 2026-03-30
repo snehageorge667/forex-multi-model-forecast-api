c90436bfbb6c9c0112f40040217a992c73a9516e
Sneha George <snehageorge667@gmail.com>
2026-03-30 18:18:04 +0530

    cleanup: removed unused start.sh

--- main.py
+++ main.py
@@ -334,4 +334,5 @@ def predict_future(
 # ==============================
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=10000)
+    import os
+    uvicorn.run("main:app", host="0.0.0.0", port=int(os.environ.get("PORT", 10000)))
